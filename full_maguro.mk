d081ac552707c658e062a847d2b4cbfb240aebf2
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-08 07:23:38 -0600

    maguro: remove Gallery2 locally

--- full_maguro.mk
+++ full_maguro.mk
@@ -18,10 +18,6 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Camera
-PRODUCT_PACKAGES := \
-    Gallery2
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
